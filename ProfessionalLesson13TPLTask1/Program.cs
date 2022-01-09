302cd67d7cb79372b06a825293b6924ec126a80e
Nikita Lebedenko <nlebn001@gmail.com>
2022-01-09 16:41:34 +0100

    Add project files.

---
+++ ProfessionalLesson13TPLTask1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProfessionalLesson13TPLTask1
+{
+    internal class Program
+    {
+        static int[] arr = new int[1000000];
+        static Random rand = new Random();
+        static void Main(string[] args)
+        {
+            ParralelArrInitialize(arr);
+            ParallelQuery<int> collection = ParallelGetOddNumsFromArray(arr);
+            ParallelConsoleOut(collection);
+        }
+
+        static void ParralelArrInitialize(int[] arr)
+        {
+            Parallel.For(0, arr.Length, (i) => arr[i] = rand.Next());
+        }
+
+        static ParallelQuery<int> ParallelGetOddNumsFromArray(int[] arr)
+        {
+            return from number in arr.AsParallel()
+                   where number % 2 != 0
+                   select number;
+
+        }
+
+        static void ParallelConsoleOut(ParallelQuery<int> collection)
+        {
+            Parallel.ForEach(collection, number => Console.WriteLine(number));
+        }
+
+    }
+}
---
+++ ProfessionalLesson13TPLTask2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProfessionalLesson13TPLTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.MaxDegreeOfParallelism = 2;
+
+            Task t1 = new Task(() => Parallel.Invoke(parallelOptions, Plus, Minus));
+            t1.Start();
+            Console.WriteLine("Main");
+            t1.Wait();
+
+        }
+
+        static void Plus()
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write("+ ");
+            }
+
+        }
+        static void Minus()
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write("- ");
+            }
+        }
+    }
+}
